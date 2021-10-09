a9080b294359bbbd758127c633914a94d4724e29
Abdan Zam Zam Ramadhan <rabdanzamzam96@gmail.com>
2021-10-09 20:14:57 +0700

    Initialize zerone dashboard for frontend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-     res.send('Hello World');
-})
+app.use(routes);
 
 app.listen(port, () => {
      console.log(`App listening at http://localhost:${port}`);
---
+++ backend/routes/index.js
@@ -0,0 +1,8 @@
+const routes = require('express').Router();
+
+routes.get('/', (req, res) => {
+     res.send('Hello World');
+});
+
+module.exports = routes;
+
